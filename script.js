e200ffe9acf683a7a94b44f214c59f65c3d84ceb
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-06-11 21:17:52 -0400

    function checkWinner done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 - Make an array with the options
 - Create a function for the computer choice
 - Make a random choice for the the computer going through the array
+- Write a function that plays a single round of Rock Paper Scissors
 */
 
 const options = ["rock", "paper", "scissors"];
@@ -10,5 +11,24 @@ function getComputerChoice() {
   const choice = options[Math.floor(Math.random() * options.length)];
   return choice;
 }
+
+function checkWinner (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "player";
+    }
+    else {
+        return "computer";
+    }
+} 
+
+function playRound(playerSelection, computerSelection) {
+    
+  }
 
 getComputerChoice();
