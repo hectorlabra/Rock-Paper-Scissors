33615534b1e287319d2e0f09b75f1994a0e60b65
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-10-05 18:34:34 -0300

    Added winning condition and game reset functionality

--- script.js
+++ script.js
@@ -1,51 +1,81 @@
-const rockButton = document.getElementById('rock-button');
-const paperButton = document.getElementById('paper-button');
-const scissorsButton = document.getElementById('scissors-button');
-const resultText = document.getElementById('result-text');
-const scoreText = document.getElementById('score-text');
+const rockButton = document.getElementById("rock-button");
+const paperButton = document.getElementById("paper-button");
+const scissorsButton = document.getElementById("scissors-button");
+const resultText = document.getElementById("result-text");
+const scoreText = document.getElementById("score-text");
 
 let playerScore = 0;
 let computerScore = 0;
 
 function playRound(playerSelection) {
-    const computerSelection = getComputerChoice();
-    const result = determineWinner(playerSelection, computerSelection);
-    updateGameResult(playerSelection, computerSelection, result);
-    updateScore(result);
+  const computerSelection = getComputerChoice();
+  const result = determineWinner(playerSelection, computerSelection);
+  updateGameResult(playerSelection, computerSelection, result);
+  updateScore(result);
 }
 
 function determineWinner(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
 
-    if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-        return 'You Win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection === computerSelection) {
-        return 'Draw';
-    } else {
-        return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
-    }
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+  } else if (playerSelection === computerSelection) {
+    return "Draw";
+  } else {
+    return "You Lose! " + computerSelection + " beats " + playerSelection;
+  }
 }
 
 function updateGameResult(playerSelection, computerSelection, result) {
-    resultText.textContent = 'Result: ' + result;
-    console.log('Player Selection: ' + playerSelection);
-    console.log('Computer Selection: ' + computerSelection);
-    console.log('Result: ' + result);
+  resultText.textContent = "Result: " + result;
+  console.log("Player Selection: " + playerSelection);
+  console.log("Computer Selection: " + computerSelection);
+  console.log("Result: " + result);
 }
 
 function updateScore(result) {
-    if (result.includes('Win')) {
-        playerScore++;
-    } else if (result.includes('Lose')) {
-        computerScore++;
-    }
+  if (result.includes("Win")) {
+    playerScore++;
+  } else if (result.includes("Lose")) {
+    computerScore++;
+  }
 
-    scoreText.textContent = 'Score: Player ' + playerScore + ' - Computer ' + computerScore;
+  scoreText.textContent =
+    "Score: Player " + playerScore + " - Computer " + computerScore;
+}
+
+// ... (código previo)
+
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
+  const result = determineWinner(playerSelection, computerSelection);
+  updateGameResult(playerSelection, computerSelection, result);
+  updateScore(result);
+
+  // Check if one of the players reached 5 points
+  if (playerScore === 5) {
+    announceWinner("Player");
+    resetGame();
+  } else if (computerScore === 5) {
+    announceWinner("Computer");
+    resetGame();
+  }
+}
+
+function announceWinner(winner) {
+  resultText.textContent = winner + " wins the game!";
+  console.log(winner + " wins the game!");
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  scoreText.textContent = "Score: Player 0 - Computer 0";
 }
 
 // ... (rest of your JavaScript code)
