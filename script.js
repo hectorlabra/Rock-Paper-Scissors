af21cada8ea6e7efb2dc850d4017754f05cdfebf
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-10-05 18:31:24 -0300

    Added buttons for player selection with event listeners

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// let's create rock paper scissors game in javascript console
-// Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
+const rockButton = document.getElementById('rock-button');
+const paperButton = document.getElementById('paper-button');
+const scissorsButton = document.getElementById('scissors-button');
+const resultText = document.getElementById('result-text');
+const scoreText = document.getElementById('score-text');
 
-
-
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -14,8 +18,6 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
-//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
 
 function playRound(playerSelection, computerSelection) {
@@ -38,7 +40,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
 function game() {
     let playerScore = 0;
@@ -76,5 +77,5 @@ function game() {
     }
 }
 
-// Call the game function to start playing
+
 game();
